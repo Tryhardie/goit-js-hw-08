5fcab93acbb47e36b962566c3587da9f3c088645
Tryhardie <realkalin@gmail.com>
2023-02-07 16:14:03 +0200

    1 task complete

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const galleryItemsMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+        <img class="gallery__image"
+        src="${preview}"
+        alt="${description}"/>
+      </a>`
+  )
+  .join('');
+
+galleryRef.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+
+const galleryLightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
